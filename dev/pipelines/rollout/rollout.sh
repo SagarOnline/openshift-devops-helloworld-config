985a2a7f8a66c94a329c476bcf9bdd04d69767f7
SagarOnline <chopade.sagar@gmail.com>
2019-08-05 18:59:07 +0530

    updated

--- dev/pipelines/rollout/rollout.sh
+++ dev/pipelines/rollout/rollout.sh
@@ -22,9 +22,5 @@ if [[ $deployments == *"jenkins"* ]]; then
 else
     oc new-app jenkins-ephemeral
 fi
-# Run Jenkins pod inside the project. It is used to run pipeline which builds application.
 
-#oc new-app jenkins-ephemeral
-
-# Process Release Management Template to create a release for helloworld v1. This will create a "helloworld-v1-dev-rollout" pipeline in cluster to deploy a helloworld application in cluster.
 oc process -f dev/pipelines/rollout/application-template.yaml --param-file application.properties --ignore-unknown-parameters=true | oc apply -f -
--- it/pipelines/promote/promote.sh
+++ it/pipelines/promote/promote.sh
@@ -1,24 +1,33 @@
-#login to openshift cluster
+#!/bin/bash
+
 oc login -u developer -p developer
 
-# create new project for 'helloworld' application release 'v1' 
-oc project helloworld-v1-dev
+devProjectName="helloworld-v1-dev"
+itProjectName="helloworld-v1-it"
 
-#oc new-project helloworld-v1-it
+oc get project > projects.txt
+projects=$(<projects.txt)
 
+if [[ $projects == *$itProjectName* ]]; then
+  oc project $itProjectName
+else
+    oc new-project $itProjectName
+fi
 
-oc get dc,svc,route,is -o=yaml  --export -l app=helloworld,release=v1,environment=dev > state.yaml
+oc project $devProjectName
 
+oc get dc,svc,route,is -o=yaml  --export -l app=helloworld,release=v1,environment=dev >state.yaml
 
-
-# Process Release Management Template to create a release for helloworld v1. This will create a "helloworld-v1-dev-rollout" pipeline in cluster to deploy a helloworld application in cluster.
-
-sed -i 's/namespace:.*//g' state.yaml
+sed -i 's/namespace:.*//g' state.yaml 
 sed -i 's/clusterIP:.*//g' state.yaml
 sed -i 's/host:.*//g' state.yaml
-# remove annotations
+                    
+                   
 sed -i 's/annotations:*//g' state.yaml
 sed -i 's/.*last-applied-configuration.*//g' state.yaml
-sed -i 's/.*{".*}//g' state.yaml
-oc project helloworld-v1-it
+sed -i 's/.*{\".*}//g' state.yaml
+#sed -i 's/.*host.generated.*//g' state.yaml
+                    
+oc project $itProjectName
+
 oc apply -f state.yaml
