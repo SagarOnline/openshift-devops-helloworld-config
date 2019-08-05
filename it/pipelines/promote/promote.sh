8cae6cc659a33584b95769a6befd41d015dc345c
SagarOnline <chopade.sagar@gmail.com>
2019-08-05 14:10:38 +0530

    updated image stream and deployemnt promotion

--- it/pipelines/promote/promote.sh
+++ it/pipelines/promote/promote.sh
@@ -4,11 +4,13 @@ oc login -u developer -p developer
 # create new project for 'helloworld' application release 'v1' 
 oc new-project helloworld-v1-it
 
+#oc project helloworld-v1-it
+
 
 oc get dc,svc,route,is -o=yaml  --export -l app=helloworld,release=v1,environment=dev > state.yaml
 
 # Process Release Management Template to create a release for helloworld v1. This will create a "helloworld-v1-dev-rollout" pipeline in cluster to deploy a helloworld application in cluster.
 
 sed -i 's/namespace:.*//g' state.yaml
-
+sed -i 's/clusterIP:.*//g' state.yaml
 oc apply -f state.yaml
